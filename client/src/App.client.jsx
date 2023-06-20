87107da5eabb34b68284bc52f81a0f304edb4d8a
MaiT-027 <mytmicekorea@naver.com>
2023-06-20 13:06:22 +0900

    .

--- client/src/App.client.jsx
+++ client/src/App.client.jsx
@@ -8,13 +8,13 @@ const App = () => {
   return (
     <>
       <h1>SDVXInfo</h1>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
           <Route path={"/"} element={<SongTable />}></Route>
           <Route path={"/song/*"} element={<SongInfo />}></Route>
           <Route path={"/score"} element={<ScoreInfo />}></Route>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </>
   );
 };
